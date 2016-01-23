c2253550ba02cc45b31410a19eb9a6bd218757ea
startsmuki <startsmuki@Gmail.com>
2016-01-24 02:01:10 +0700

    Installation of Bootstrap
    Enjoy and start working

---
+++ wordpress template/footer.php
@@ -0,0 +1,7 @@
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+</body>
+<footer>
+</footer>
+</html>
