a59858a1cf53f32b15a711a5b2efac5a18ad2603
Joshua C <jrandiny2@gmail.com>
2016-01-24 15:55:58 +0700

    Basic header

--- wordpress template/header.php
+++ wordpress template/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title(''); ?></title>
+
     <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -12,3 +13,4 @@
     <![endif]-->
   </head>
   <body> <!-- body tag opens in header.php and ends in footer.php -->
+  <p>header</p>
