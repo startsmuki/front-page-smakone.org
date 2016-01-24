0523ea422fd02e3333dd0cc721c507f9cd9f066b
Joshua C <jrandiny2@gmail.com>
2016-01-24 15:56:13 +0700

    Tambahin footer

--- wordpress template/footer.php
+++ wordpress template/footer.php
@@ -1,7 +1,25 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
-</body>
+
 <footer>
+<div class="container-fluid">
+		<div class="row">
+			<div id="footlink" class="col-xs-12">
+				<p style="margin: 10px 0 10px 0;">&copy; 2015 START</p>
+			</div>
+		</div>
+		<div class="row">
+			<div id="footlink" class="col-sm-10">
+					<a href="">footer 1</a>
+					<a href="">footer 1</a>
+					<a href="">footer 1</a>
+			</div>
+			<div class="col-sm-2 text-center">
+				<p>some content</p>
+			</div>
+		</div>
+	</div>
 </footer>
+</body>
 </html>
