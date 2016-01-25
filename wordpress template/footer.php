9ae1677f08e9452b381e5a26a6fb102cf9958643
Joshua C <jrandiny2@gmail.com>
2016-01-25 13:30:55 +0700

    fix closing tag missing

--- wordpress template/footer.php
+++ wordpress template/footer.php
@@ -1,32 +1,31 @@
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
 
 <footer>
-<!--TITLE FOOTER-->
-<div class="container-fluid">
+	<div class="container-fluid">
 		<div class="row">
 			<div id="footlink" class="col-xs-6">
 				<p style="margin: 10px 0 10px 0;"><strong>&copy; 2015 START</strong></p>
 			</div>
-			<div id="footlink2" class="col-xs-6"><p style="margin: 10px 0 10px 0;"><strong>SOCIAL MEDIA</strong></p>
+			<div id="footlink2" class="col-xs-6">
+					<p style="margin: 10px 0 10px 0;">
+						<strong>SOCIAL MEDIA</strong>
+					</p>
 			</div>
 		</div>
-		<!--END OF TITLE OF FOOTER-->
 		<div class="row">
 			<div id="footlink" class="col-sm-6">
 					<a href="">LOCATION</a>
 					<a href="">CONTACT INFO</a>
 					<a href="">footer 3</a>
-					</div>
-		<div class="row">
+			</div>
 			<div id="footlink2" class="col-sm-6">
 					<a href="">Socmed1</a>
 					<a href="">Socmed2</a>
 					<a href="">Socmed3</a>
-					</div>
-			
-			
+			</div>
+		</div>
+	</div>	
 </footer>
 </body>
 </html>
