b88f8b21292b22af69491b4239b04f77513d9aef
Joshua C <jrandiny2@gmail.com>
2016-01-29 17:13:12 +0700

    Buat footer mobile friendly... YAY!!

--- wordpress template/footer.php
+++ wordpress template/footer.php
@@ -2,30 +2,27 @@
 <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
 
 <footer>
-	<div class="container-fluid">
-		<div class="row">
-			<div id="footlink" class="col-xs-6">
-				<p style="margin: 10px 0 10px 0;"><strong>&copy; 2015 START</strong></p>
-			</div>
-			<div id="footlink2" class="col-xs-6">
-					<p style="margin: 10px 0 10px 0;">
-						<strong>SOCIAL MEDIA</strong>
-					</p>
-			</div>
-		</div>
-		<div class="row">
-			<div id="footlink" class="col-sm-6">
-					<a href="">LOCATION</a>
-					<a href="">CONTACT INFO</a>
-					<a href="">footer 3</a>
-			</div>
-			<div id="footlink2" class="col-sm-6">
-					<a href="">Socmed1</a>
-					<a href="">Socmed2</a>
-					<a href="">Socmed3</a>
-			</div>
-		</div>
-	</div>	
+    <div class="container-fluid">
+        <div class="row">
+            <div id="footer-kiri" class="col-sm-6 footer-link">
+                    <p style="margin: 10px 0 10px 0;">
+                        <strong>&copy; 2015 START</strong>
+                    </p>
+                    <a href="">LOCATION</a>
+                    <a href="">CONTACT INFO</a>
+                    <a href="">footer 3</a>
+            </div>
+            <div id="footer-kanan" class="col-sm-6 footer-link">
+                <p style="margin: 10px 0 10px 0;">
+                    <strong>SOCIAL MEDIA</strong>
+                </p>
+                <a href="">Socmed1</a>
+                <a href="">Socmed2</a>
+                <a href="">Socmed3</a>
+            </div>
+        </div>
+
+    </div>
 </footer>
 </body>
 </html>
